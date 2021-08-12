633086f5724de56196ad7e954f5fc6c2d4270e7c
Crowdbotics <team@crowdbotics.com>
2021-08-12 02:33:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "silent_dream_29746Identifier",
+  url: "https://silent_dream_29746Identifier.botics.co",
+  api: "https://silent_dream_29746Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
